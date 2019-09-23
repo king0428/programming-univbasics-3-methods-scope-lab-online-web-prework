b926970478b2a06ed63cd07a17c1a2611082cc0d
Geovany Valdovinos <brownvdoe@gmail.com>
2019-09-23 20:52:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -38,7 +38,7 @@ end
   describe "any_phrase" do
     it "takes in an argument and puts out the catch phrase" do
       phrase = "Do A Barrel Roll!"
-      expect{any_phrase}.to output("Do A Barrel Roll!\n").to_stdout
+      expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
     end
   end
     
